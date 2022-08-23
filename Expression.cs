2e42679e5fe9a1d898749f40609023cdc9555576
ilia <ilia@DESKTOP-4E6FF6Q>
2022-08-23 11:39:04 +0300

    Добавьте файлы проекта.

---
+++ Expression.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Expression
+    {
+        public static float Evaluate(String expression)
+        {
+            Stack<float> stack = new Stack<float>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (Char.IsDigit(expression[i]))
+                {
+                    int startIndex = i;
+                    while (Char.IsDigit(expression[i]))
+                    {
+                        i++;
+                    }
+                    string substr = expression.Substring(startIndex,i-startIndex);
+                    stack.Push(float.Parse(substr));
+                    i--;
+                }
+                else if (expression[i] == '+' ||
+                         expression[i] == '-' ||
+                         expression[i] == '/' ||
+                         expression[i] == '*')
+                {
+                    float a = stack.Pop();
+                    float b = stack.Pop();
+                    float res = 0;
+                    switch (expression[i])
+                    {
+                        case '+': res = b + a; break;
+                        case '-': res = b - a; break;
+                        case '*': res = b * a; break;
+                        case '/': res = b / a; break;
+                    }
+                    stack.Push(res);
+                }
+            }
+            return stack.Peek();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                try
+                {
+                    String input = Console.ReadLine();
+                    float res = Expression.Evaluate(input);
+                    Console.WriteLine(res);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("некорректный ввод");
+                }
+            }
+        }
+    }
+}
